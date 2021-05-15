432ffae81b644def7a83fcd974b61ad0c895c29c
Lavanya <Lavanya@DESKTOP-F8AM24T>
2021-05-15 15:52:08 +0530

    edit expense service implementation modified

--- src/main/java/com/rs/fer/controller/FERController.java
+++ src/main/java/com/rs/fer/controller/FERController.java
@@ -125,7 +125,7 @@ public class FERController {
 			return new ResponseEntity<Expense>(expense, HttpStatus.OK);
 		}else 
 		{
-			return new ResponseEntity<Expense>(expense, HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<Expense>(expense, HttpStatus.INTERNAL_SERVER_ERROR);
 		 }
 		}
 }
